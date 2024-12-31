55a81a59a6c36c4b7a1ab725ce87dc1c70fec455
getmahbub <64847914+getmahbub@users.noreply.github.com>
2025-01-01 03:41:09 +1100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
+
 // Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+ // output: 'server',
+ // adapter: vercel(),
 });
