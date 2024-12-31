89b93d197e2f16dab00093a0ff95b44bcb0228c2
getmahbub <64847914+getmahbub@users.noreply.github.com>
2025-01-01 03:26:05 +1100

    update

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_BZY7-NwK.mjs';
-import { manifest } from './manifest_B9KTp3AP.mjs';
+import { manifest } from './manifest_BD4yS8i3.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/404.astro.mjs');
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "4045c82d-5bd8-493b-9656-2d2f941903b8",
+    "middlewareSecret": "de1f0608-dd33-4a0e-a3c5-d752a0478a2c",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
