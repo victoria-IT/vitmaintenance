8c7e5c78566a653cdf83a1d5e418a4cc21b0479d
getmahbub <64847914+getmahbub@users.noreply.github.com>
2025-04-26 15:24:44 +1000

    cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+//import netlify from '@astrojs/netlify';
+
+import cloudflare from '@astrojs/cloudflare';
 
 
 // Import the Vercel adapter
 // import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
+
 export default defineConfig({
+  // output: 'server',
+  // adapter: vercel(),
   integrations: [tailwind()],
- // output: 'server',
- // adapter: vercel(),
+
+  adapter: cloudflare(),
 });
