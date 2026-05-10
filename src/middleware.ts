a6ba7b41d1cd754dcbd36ada7747b526d2c664a7
getmahbub <64847914+getmahbub@users.noreply.github.com>
2026-05-10 23:25:31 +1000

    Add X-Robots-Tag header to pages.dev redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,9 @@ const PROD_ORIGIN = "https://maintenance.victoriait.com";
 export const onRequest = defineMiddleware((context, next) => {
   if (context.url.hostname.toLowerCase() === DEV_HOST) {
     const target = new URL(`${context.url.pathname}${context.url.search}${context.url.hash}`, PROD_ORIGIN);
-    return Response.redirect(target.toString(), 301);
+    const response = Response.redirect(target.toString(), 301);
+    response.headers.set("X-Robots-Tag", "noindex, nofollow");
+    return response;
   }
 
   return next();
